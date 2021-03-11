11f2e7a901e90df2f215f55c16334cdfa17a49a5
ngwatso <ngwatson79@yahoo.com>
2021-03-10 22:12:39 -0500

    started server, endpoints sketched out

--- api/server.js
+++ api/server.js
@@ -1,3 +1,25 @@
-// BUILD YOUR SERVER HERE
+// !! BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// ** Imports
+const express = require('express');
+
+// ** Express
+const server = express();
+
+// ** Middleware
+server.use(express.json());
+
+// ** Endpoints
+
+// ?? POST ==> /api/users ==> create user
+
+// ?? GET ==> /api/users ==> return array
+
+// ?? GET ==> /api/users/:id ==> return user object
+
+// ?? DELETE ==> /api/users/:id ==> remove user
+
+// ?? PUT ==> /api/users/:id ==> update user
+
+// !! EXPORT YOUR SERVER instead of {}
+module.exports = server;
